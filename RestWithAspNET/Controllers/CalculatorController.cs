4abcbac6459d9652ad564894287d6b5ddb124ca5
Adri Silva <adri.silva@catskillet.com>
2021-04-01 10:06:04 -0300

    feat: create simple calculator with basic operations

---
+++ RestWithAspNET/Controllers/CalculatorController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("basic-operation/{operation}/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string operation, string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber))
+            {
+                return BadRequest("Invalid numbers!");
+            }
+
+            decimal total; 
+            decimal firstNumberDecimal = ConvertToDecimal(firstNumber);
+            decimal secondNumberDecimal = ConvertToDecimal(secondNumber);
+
+            switch (operation)
+            {
+                case "sum":
+                    total = firstNumberDecimal + secondNumberDecimal;
+                    break;
+                case "subtraction":
+                    total = firstNumberDecimal - secondNumberDecimal;
+                    break;
+                case "multiplication":
+                    total = firstNumberDecimal * secondNumberDecimal;
+                    break;
+                case "division":
+                    total = firstNumberDecimal / secondNumberDecimal;
+                    break;
+                case "mean":
+                    total = (firstNumberDecimal + secondNumberDecimal) / 2;
+                    break;
+                default:
+                    return BadRequest("Invalid Operation!");
+            }
+
+            return Ok(total);
+        }
+        
+        [HttpGet("basic-operation/square-root/{firstNumber}")]
+        public IActionResult Get(string firstNumber)
+        {
+            if (!IsNumeric(firstNumber))
+            {
+                return BadRequest("Invalid number!");
+            }
+
+            decimal firstNumberDecimal = ConvertToDecimal(firstNumber);
+            double total = Math.Sqrt((double) firstNumberDecimal); 
+
+            return Ok(total);
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber,
+                NumberStyles.Any,
+                NumberFormatInfo.InvariantInfo,
+                out number);
+            
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal number;
+
+            if (decimal.TryParse(strNumber, out number))
+            {
+                return number;
+            }
+
+            return 0;
+        }
+    }
+}
--- RestWithAspNET/Startup.cs
+++ RestWithAspNET/Startup.cs
@@ -27,10 +27,6 @@ namespace RestWithAspNET
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "RestWithAspNET", Version = "v1"});
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,8 +35,6 @@ namespace RestWithAspNET
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestWithAspNET v1"));
             }
 
             app.UseHttpsRedirection();
