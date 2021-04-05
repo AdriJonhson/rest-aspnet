d47733dbe403ca5277bb5bec3cfd6dc678e7128f
Adri Silva <adri.silva@catskillet.com>
2021-04-05 10:45:17 -0300

    fix: config JsonProperty

--- RestWithAspNET/Data/VO/PersonVO.cs
+++ RestWithAspNET/Data/VO/PersonVO.cs
@@ -1,11 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace RestWithAspNET.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("id")]
         public long Id { get; set; }
+
+        [JsonPropertyName("first_name")]
         public string FirstName { get; set; }
+        
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+        
+        [JsonPropertyName("address")]
         public string Address { get; set; }
+        
+        [JsonPropertyName("gender")]
         public string Gender { get; set; }
     }
 }
