f7c7a2223bb5456dcc107067c5f6a1dfd445cb7a
Adri Silva <adri.silva@catskillet.com>
2021-04-08 16:41:19 -0300

    feat: implementing get file

--- RestWithAspNET/Business/Implemetations/FileBusiness.cs
+++ RestWithAspNET/Business/Implemetations/FileBusiness.cs
@@ -19,7 +19,8 @@ namespace RestWithAspNET.Business.Implemetations
 
         public byte[] GetFile(string filename)
         {
-            throw new System.NotImplementedException();
+            var filePath = _basePath + filename;
+            return File.ReadAllBytes(filePath);
         }
 
         public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
--- RestWithAspNET/Controllers/FileController.cs
+++ RestWithAspNET/Controllers/FileController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -37,6 +38,23 @@ namespace RestWithASPNet.Controllers
             List<FileDetailVO> details = await _fileBusiness.SaveFilesToDisk(files);
 
             return new ObjectResult(details);
+        }
+        
+        [HttpGet("downloadFile/{fileName}")]
+        [Authorize("Bearer")]
+        public async Task<IActionResult> GetFile(string fileName)
+        {
+            byte[] buffer = _fileBusiness.GetFile(fileName);
+
+            if (buffer != null)
+            {
+                HttpContext.Response.ContentType = $"application/{Path.GetExtension(fileName).Replace(".", "")}";
+                HttpContext.Response.Headers.Add("context-length", buffer.Length.ToString());
+                HttpContext.Response.Headers.Append("content-type", "image/jpeg");
+                await HttpContext.Response.Body.WriteAsync(buffer, 0, buffer.Length);
+            }
+
+            return new ContentResult();
         }
     }
 }
