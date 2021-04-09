a4dbc31d231b4aa6bba6b0d3a71ca559470fc4cf
Adri Silva <adri.silva@catskillet.com>
2021-04-09 10:03:28 -0300

    feat: configuring docker support

--- RestWithAspNET/Startup.cs
+++ RestWithAspNET/Startup.cs
@@ -94,10 +94,10 @@ namespace RestWithAspNET
             var connection = Configuration["MysqlConnection:MysqlConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
-            if (Environment.IsDevelopment())
-            {
-                MigrateDatabase(connection);
-            }
+            // if (Environment.IsDevelopment())
+            // {
+            //     MigrateDatabase(connection);
+            // }
 
             services.AddMvc(options =>
             {
